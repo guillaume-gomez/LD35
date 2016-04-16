5f19fe60f5bc1d4b5b209cf36a621f74f38b03ca
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-04-16 13:18:51 +0200

    auto creation level works :)

--- js/tileset.js
+++ js/tileset.js
@@ -2,6 +2,8 @@
 * @brief : Classe qui gere le niveau
 **/
 
+const FLOOR_Y = 570
+
 function TileSet (viewport,cell_size )
 {   
     var m_viewport;
@@ -15,9 +17,6 @@ function TileSet (viewport,cell_size )
         m_tile_map = new jaws.TileMap({size : [m_viewport.max_x/cell_size+10,m_viewport.max_y/cell_size+10] , cell_size: [cell_size,cell_size]});
         m_spriteList = new jaws.SpriteList();
         m_max_width_created = 0;
-        //this.loadLevel();
-        this.createFloor(0);
-        this.createFloor(m_viewport.width);
     }
     
     this.getSpriteList = function() {
@@ -41,21 +40,27 @@ function TileSet (viewport,cell_size )
          m_currentLevel++;
     }
 
-    this.loadLevel = function() {
-       m_spriteList = new jaws.SpriteList();
-       m_spriteList.load(jaws.assets.get("level"+m_currentLevel+".json")); 
-        m_tile_map = new jaws.TileMap({size : [m_viewport.max_x/cell_size+10,m_viewport.max_y/cell_size+10] ,cell_size: [cell_size,cell_size]});
-        m_tile_map.push(m_spriteList);
-    }
-
     this.deleteTiles = function(viewport) {
+        var deletion = false;
         for(var col=0; col < m_tile_map.cells.length; col++) {
            for(var row=0; row < m_tile_map.cells[col].length; row++) {
                 var item = m_tile_map.cells[col][row];
                 if (item.length > 0 && viewport.isLeftOf(item[0])) {  
                     m_tile_map.cells[col].splice(row, 1);
+                    deletion = true;
                 }
             }
+        }
+        return deletion;
+    }
+
+    this.manageTiles = function(viewport) {
+        //delete unecessary tiles
+        this.deleteTiles(viewport);
+        //add new one
+        if (m_max_width_created < viewport.x + viewport.width) {
+            debugger
+            m_max_width_created = this.createFloor(m_max_width_created + 20);
         }
     }
 
@@ -64,9 +69,10 @@ function TileSet (viewport,cell_size )
         debugger
         for (var x = 0; x < m_viewport.width; x += cell_size) {
 
-            blocks.push( new Sprite({image: "test.png", x: offset + x, y: 570}));
+            blocks.push( new Sprite({image: "test.png", x: offset + x, y: FLOOR_Y}));
         }
         m_tile_map.push(blocks);
+        return offset + m_viewport.width;
     }
 
     this.addTiles = function(viewport) {
