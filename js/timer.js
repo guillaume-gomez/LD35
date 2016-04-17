1931717a37007468f7e3d8120414e6f7cf10aa8b
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-04-17 02:33:34 +0200

    fix timer

--- js/timer.js
+++ js/timer.js
@@ -1,73 +1,27 @@
-function Timer ()
+Timer = function ()
 {
+    this.lastTime = (new Date()).getTime();
+    this.totalTime = 0;
+    this.updateTime = 0;
+    this.delta = 0 ;
+}
+    
+Timer.prototype.start = function () {
+    this.updateTime = 0 ;
+}
 
-    ///////////////////////////////////////
-    // Attributs
-    ///////////////////////////////////////
-    var delta;
-    var lastTime;
-    var totalTime;
-    var updateTime;
+Timer.prototype.reset = function() {
+    this.lastTime = (new Date()).getTime();
+    this.totalTime = 0;
+    this.updateTime = 0;
+    this.delta = 0 ;
+}
 
-    ///////////////////////////////////////
-    // Méthodes
-    //////////////////////////////////////
-    
-    /**
-    *@brief : Constructeur de la classe Timer
-    **/
-    this.constructor = function()
-    {
-        lastTime = (new Date()).getTime();
-        totalTime = 0;
-        updateTime = 0;
-        delta = 0 ;
-    }
-    
-    /**
-    *@brief : Méthode qui lance le timer ou le relance
-    **/
-    this.lancer = function ()
-    {
-        updateTime = 0 ;
-        updateFrames = 0 ;
-    }
-    
-    /**
-    *@brief : Reset 
-    **/
-    this.reset = function()
-    {
-        lastTime = (new Date()).getTime();
-        totalTime = 0;
-        updateTime = 0;
-        delta = 0 ;
-    }
-    
-    
-    this.calculIntervalle = function ()
-    {
-        var  now = (new Date()).getTime();
-        delta = now - lastTime ;
-        lastTime = now ;
-        totalTime += delta  ;
-        updateTime += delta ;
-    }
-    
-    
-    /**
-    *@brief : Accesseur qui permet d'obtenir la difference de temps entre maintenant et le lancement
-    **/
-    this.getIntervalle = function ()
-    {
-        var  now = (new Date()).getTime();
-        delta = now - lastTime ;
-        lastTime = now ;
-        totalTime += delta  ;
-        updateTime += delta ;
-        return updateTime;
-    }
-
-
-//end of class
+Timer.prototype.getInterval = function () {
+    var now = (new Date()).getTime();
+    this.delta = now - this.lastTime ;
+    this.lastTime = now ;
+    this.totalTime += this.delta  ;
+    this.updateTime += this.delta ;
+    return this.updateTime;
 }
